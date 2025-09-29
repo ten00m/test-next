045fe2e42edb063c70bf8e5b21aafd0807aa2963
ten00m <ilya.safronov.1989@gmail.com>
2025-09-29 21:59:02 +0300

    Adding file input to ui kit

---
+++ src/shared/ui/fileInput/FileInput.tsx
@@ -0,0 +1,22 @@
+import {FC, InputHTMLAttributes} from "react";
+import {v4 as uuid} from "uuid";
+
+interface FileInputProps extends InputHTMLAttributes<HTMLInputElement> {
+    label?: string;
+}
+
+export const FileInput: FC<FileInputProps> = ({ label, className = '', ...props }) => {
+    const id = uuid();
+
+    return (
+        <div className="relative flex flex-col pt-2.5">
+            {label && <label className="absolute h-5 w-15 text-center text-5 text-sm font-medium text-text-secondary bg-background top-0 left-2.5" id={id}>{label}</label>}
+            <input
+                type="file"
+                className={`px-3 py-2 border-2 border-border rounded-lg focus:outline-none focus:border-primary ${className}`}
+                id={id}
+                {...props}
+            />
+        </div>
+    );
+};
---
+++ src/shared/ui/fileInput/index.ts
@@ -0,0 +1,1 @@
+export {FileInput as defautl} from './FileInput'
