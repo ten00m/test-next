69f56b3e16abd1b183a7d883cd0ba5a59b1693d7
ten00m <ilya.safronov.1989@gmail.com>
2025-09-30 06:09:27 +0300

    add argument to hook

--- src/features/modal-from/useModalForm.ts
+++ src/features/modal-from/useModalForm.ts
@@ -1,26 +1,23 @@
 'use client'
 
 import { useState } from 'react'
-import { ModalFormData } from '@/shared/lib/types/modal'
 
-export const useModalForm = () => {
+interface IUseModalFrom<T> {
+    onSubmit: (data: T) => void
+}
+
+export const useModalForm = <TData>({onSubmit}: IUseModalFrom<TData>) => {
     const [isModalOpen, setIsModalOpen] = useState(false)
     const [isLoading, setIsLoading] = useState(false)
 
     const openModal = () => setIsModalOpen(true)
     const closeModal = () => setIsModalOpen(false)
 
-    const handleSubmit = async (data: ModalFormData) => {
+    const handleSubmit = async (data: TData) => {
         setIsLoading(true)
 
         try {
-            // Здесь будет логика отправки данных
-            console.log('Отправка данных формы:', data)
-
-            // Имитация задержки API
-            await new Promise((resolve) => setTimeout(resolve, 1000))
-
-            console.log('Форма успешно отправлена!')
+            onSubmit(data);
             closeModal()
         } catch (error) {
             console.error('Ошибка при отправке формы:', error)
