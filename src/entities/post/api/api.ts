8d208e8d8201ca494b451def3ccfbce5238303b1
ten00m <ilya.safronov.1989@gmail.com>
2025-09-30 04:57:07 +0300

    add createPost function

--- src/entities/post/api/api.ts
+++ src/entities/post/api/api.ts
@@ -1,12 +1,12 @@
 import { createApi } from '@/src/shared/api'
 import { Post } from '@/src/entities/post/types/types'
 
-export interface GetPosts {
+export interface IPosts {
     posts: Post[]
     error: string | null
 }
 
-export const getPosts = async (): Promise<GetPosts> => {
+export const getPosts = async (): Promise<IPosts> => {
     const { error, get } = createApi()
     const posts = await get('/posts')
 
@@ -18,6 +18,28 @@ export const getPosts = async (): Promise<GetPosts> => {
     } else {
         return {
             posts: posts as Post[],
+            error: null,
+        }
+    }
+}
+
+interface IPost {
+    post: Post | undefined;
+    error: string | null;
+}
+
+export const createPost = async (postData: Omit<Post, 'id'>): Promise<IPost> => {
+    const { error, post } = createApi()
+    const createdPost = await post('/posts', postData);
+
+    if (error) {
+        return {
+            post: undefined,
+            error: error,
+        }
+    } else {
+        return {
+            post: createdPost as Post,
             error: null,
         }
     }
